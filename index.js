c99d111c2872850ddb0df0f772954e1322e8ef78
Shivani_Panda <shivanipanda30@gmail.com>
2024-08-06 20:39:08 +0530

    setup codebase and added new screens

--- index.js
+++ index.js
@@ -5,5 +5,18 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import store, {persistor} from './src/reducers/reduxStore';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
-AppRegistry.registerComponent(appName, () => App);
+const MainApp = props => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App {...props} />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => MainApp);
